9f82e1570c8040b488a38e3f70677fdd70716465
Ruth Moraa <moraar37@gmail.com>
2023-02-21 13:57:25 +0300

    completed assignment

---
+++ lib/create.sql
@@ -0,0 +1,12 @@
+CREATE TABLE bears (
+  //columns here
+
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  sex TEXT,
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN
+
+);
---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) values
+(1, "Mr. Chocolate", 10, "F", "chocolate", "fierce", "TRUE"),
+(2, "Rowdy", 20, "F", "Rowdy", "goofy", "TRUE"),
+(3, "Tabitha", 30, "M", "pale", "fierce", "FALSE"),
+(4, "Sergeant Brown", 20, "M", "sergeant", "fierce", "TRUE"),
+(5, "Melissa", 30, "F", "brown", "fierce", "FALSE"),
+(6, "Grinch", 10, "M", "chocolate", "goofy", "TRUE"),
+(7, "Wendy", 40, "F", "chocolate", "strict", "TRUE"),
+(8, "Mr. Chocolate", 50, "F", "chocolate", "fierce", "TRUE"),
+(8,null, 20, "M", "black", "aggressive", 0);
